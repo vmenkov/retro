d68f20741323b8e2198ffb45c5106cc48d85b1a8
vmenkov <vmenkov@89f6c73f-c0a6-48b9-90e3-8e60578b5dbd>
2017-11-24 03:30:36 +0000

    scripts

--- coaccess-series.sh
+++ coaccess-series.sh
@@ -2,6 +2,8 @@
 
 time java  -cp ../lib/retro.jar:../lib/commons-lang-2.4.jar:../lib/commons-csv-1.1.jar \
 -Dinc=true -Dcompact=true -Dindex=out-index -Dstep=24 -Dprofile=false -Dstructure=true -Dcutoff=1 \
- edu.rutgers.retro.Coaccess urange 0 100 10
+ edu.rutgers.retro.Coaccess urange  100000 101000 10
+
+# 600  1000 10
 
 
